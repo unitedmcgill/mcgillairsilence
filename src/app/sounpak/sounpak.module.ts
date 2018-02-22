f4cad851433ce277cf20efc878540bb4b18eb855
Courtney <JCourtney@unitedmcgill.com>
2018-02-22 13:39:19 -0500

    Upgrade to Angluar5

--- src/app/sounpak/sounpak.module.ts
+++ src/app/sounpak/sounpak.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }                 from '@angular/core';
 import { CommonModule }             from '@angular/common';
 import { FormsModule }              from '@angular/forms';
-import { TabsModule }               from 'ng2-bootstrap/tabs';
+import { TabsModule }               from 'ngx-bootstrap/tabs';
 
 import { SounpakComponent }       from './sounpak.component';
 import { SounpakRoutingModule }   from './sounpak-routing.module';
--- src/app/unihousing/unihousing.module.ts
+++ src/app/unihousing/unihousing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }                 from '@angular/core';
 import { CommonModule }             from '@angular/common';
 import { FormsModule }              from '@angular/forms';
-import { TabsModule }               from 'ng2-bootstrap/tabs';
+import { TabsModule }               from 'ngx-bootstrap/tabs';
 
 import { UniHousingComponent }       from './unihousing.component';
 import { UniHousingRoutingModule }   from './unihousing-routing.module';
